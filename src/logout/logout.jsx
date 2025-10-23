b9e9e47a8e608ef2f0b6a7e743bb20f234039ed8
Bryce Taylor <bwtaylor33@gmail.com>
2025-10-22 21:35:53 -0600

    progress

---
+++ src/logout/logout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
+
+export function Logout(props) {
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+        localStorage.removeItem('userName');
+        props.onLogout();
+        navigate("/login", { replace: true });
+    }, [navigate]);
+  
+    return null;
+  }
